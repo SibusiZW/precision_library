9de121270e89a50840ea8578e359e844fb8d9d83
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-03-04 17:49:36 +0200

    registered book model on admin but not migrated yet

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    STATUSES = [
+        ('Good', 'Good'),
+        ('Borrowed', 'Borrowed'),
+        ('Bad', 'Bad'),
+    ]
+
+    name = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    status = models.CharField(max_length=100, choices=STATUSES)
+    date_issued = models.DateTimeField(auto_now_add=True)
