6e2c0579e0e83457a69fae5fd9b1ebc0dadb4b69
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-03-05 18:38:04 +0200

    added page for all books

--- core/views.py
+++ core/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from .models import Book
 
 # Create your views here.
 @login_required(login_url='/auth/login/')
 def home(request):
-    return render(request, 'home.html', { 'user': request.user })
+    books = Book.objects.all()
+    return render(request, 'home.html', { 'user': request.user, 'books': books })
 
 @login_required(login_url='/auth/login/')
 def signout(request):
